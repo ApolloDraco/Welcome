45dcc7c8497cb02b52a646f966ae24f41213aca1
Apollo <88677238+ApolloDraco@users.noreply.github.com>
2021-08-11 14:19:58 -0400

    Create Welcome.java

---
+++ Welcome.java
@@ -0,0 +1,10 @@
+/* 
+ *  Description: This application displays 'Welcome to Java!' in a message dialog box
+*/
+
+public class Welcome {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Java!"); 
+    }
+} 
+}
